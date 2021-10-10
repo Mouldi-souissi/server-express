362a8e84489329574bc7f32ec231e7dc19af236b
unknown <souissi13mouldi@gmail.com>
2021-10-10 10:02:46 +0100

    try again

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const connectDB = require("./db/db");
-const userRoute = require("./routes/user");
+const userRoute = require("./api/user");
 require("dotenv").config();
 
 // connect to DB
@@ -14,8 +14,6 @@ app.use(cors());
 
 // routes
 app.use("/api/", userRoute);
-
-app.get("/", (req, res) => res.send("hello user"));
 
 const PORT = process.env.PORT || 5000;
 
